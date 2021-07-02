da8136fdff0f7f3bf26096f8c575339618f74889
Leonardo Martins <leodsmartins@gmail.com>
2021-07-02 11:23:56 -0300

    Microdesafios de Loops

---
+++ Loop_Microdesafios.js
@@ -0,0 +1,25 @@
+// Repetir como um papagaio
+
+for(i=1; i<=5; i++){
+    let repetir = ("Olá mundo");
+    console.log(repetir);
+};
+
+//Números ímpares
+
+for (i=1; i<=10; i++){
+    if (i % 2 != 0){
+        console.log(i);
+    }
+   
+}
+
+
+//Tabuada
+for(i=1; i<=10; i++)
+    for(num=1; num<=10; num++){
+        let vezes = (i*num);
+        console.log(i + "X" + num + "=" + vezes);
+    }
+
+ 
